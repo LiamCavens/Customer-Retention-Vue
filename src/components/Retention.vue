98a9851ea819051505cabf2e3606c0a56567095d
Liam <liamcavens@googlemail.com>
2020-11-30 11:39:10 +0000

    default stage

--- src/components/Retention.vue
+++ src/components/Retention.vue
@@ -94,7 +94,7 @@ export default {
   props: {},
   data: () => {
     return {
-      manageType: "finalCancel",
+      manageType: "cancel",
       cancelReason: "",
       cancelSubReason: "",
     };
